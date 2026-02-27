04848ccfe4282790a7d8e89c3d5b2446d6272003
ariefbudiman0112 <ariefbudiman0112@gmail.com>
2026-02-27 10:42:37 +0100

    fixing out

--- authentication/middleware/auth.js
+++ authentication/middleware/auth.js
@@ -1,4 +1,5 @@
 const { verifyToken } = require('../helpers/jwt');
+const { Todo } = require('../models');
 
 const authentication = (req, res, next) => {
     try{
@@ -12,4 +13,22 @@ const authentication = (req, res, next) => {
     }
 };
 
-module.exports = {authentication};
+const authorization = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const userId = req.userData.id;
+
+        const todo = await Todo.findByPk(+id);
+        const todoFound = todo.dataValues
+        if (!todoFound) {
+           res.status(404).json({ message: 'Todo not found - Authorization' });
+        }
+        if (todo.UserId !== userId) {
+            res.status(403).json({ message: 'Forbidden - You do not have access to this resource' });
+        }
+        next();
+    }catch(error) {
+        next(error);
+    }
+};
+module.exports = {authentication, authorization};
--- authentication/routes/todo.js
+++ authentication/routes/todo.js
@@ -1,13 +1,21 @@
 const todoRouter = require('express').Router();
 const {todoController} = require('../controllers');
-const { authentication } = require('../middleware/auth');
+const { authentication, authorization } = require('../middleware/auth');
 
 
 todoRouter.get('/', todoController.getAllTodo);
 todoRouter.get('/detail/:id', todoController.getTodoById);
 todoRouter.post('/create', authentication, todoController.create);
-todoRouter.delete('/delete/:id', todoController.delete);
-todoRouter.put('/update/:id', todoController.update);
+todoRouter.delete(
+    '/delete/:id',
+     authentication, 
+     authorization, 
+     todoController.delete);
+todoRouter.put(
+    '/update/:id', 
+    authentication, 
+    authorization, 
+    todoController.update);
 
 todoRouter.get("/search", todoController.search);
 
